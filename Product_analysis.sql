16b1f54e05e5ebaeb1ee1759dd500d2a0e9c9cdc
snandhakishor <snandhakishor583@gmail.com>
2026-01-09 05:22:35 +0530

    added profit percentage by category

--- Product_analysis.sql
+++ Product_analysis.sql
@@ -8,3 +8,14 @@ select ProductKey, product_name, sum(sales) as total_sales
 from top_selling_products
 group by ProductKey, product_name
 order by total_sales desc;
+
+-- product analysis - profit percentage by category
+with category_tbl as
+(select sum((p.`Unit Price USD`-p.`Unit Cost USD`)*s.Quantity) as Profit,
+ sum(p.`Unit Price USD`*s.Quantity) as sales,
+p.CategoryKey, p.Category 
+from sales s left join dim_products p
+on s.ProductKey = p.ProductKey
+group by p.CategoryKey, p.Category)
+select CategoryKey, Category, concat(round(Profit/Sales, 2)*100, '%') as Profit_percent
+from category_tbl
