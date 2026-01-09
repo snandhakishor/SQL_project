05d3c8ec1b69bff8e984ca883da88eda5ee37d62
snandhakishor <snandhakishor583@gmail.com>
2026-01-09 23:03:13 +0530

    Added new code and its explanation

--- Customer_behavior.sql
+++ Customer_behavior.sql
@@ -14,3 +14,17 @@ from customer_orders)
 select customer_type, count(distinct CustomerKey) as `count`
 from customer_type_count
 group by customer_type;
+
+-- Most valuable and most active customers by gender 
+
+select Gender, 
+round((total_quantity/sum(total_quantity) over())*100,2) as `total_quantity %`, 
+round((total_sales/sum(total_sales) over())*100,2) as `total_sales %` from
+(select c.Gender, sum(s.Quantity) as total_quantity , 
+sum(s.Quantity*p.`Unit Price USD`) as total_sales
+from sales s inner join dim_customers c 
+on s.CustomerKey = c.CustomerKey 
+inner join dim_products p 
+on s.ProductKey = p.ProductKey
+group by Gender) as gender_table
+group by Gender
