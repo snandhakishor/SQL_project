99a163c274ec5b66a93d8bcc3c01d13a71460466
snandhakishor <snandhakishor583@gmail.com>
2026-01-10 14:56:23 +0530

    added new product analysis

--- sales_analysis.sql
+++ sales_analysis.sql
@@ -1,4 +1,15 @@
--- running total sales over month
+
+-- 1) Problem: monthly total sales
+
+select month(s.`Order Date`) as month_no,
+monthname(s.`Order Date`) as month_, 
+sum(s.Quantity*p.`Unit Price USD`) as sales
+from sales s inner join dim_products p
+on s.ProductKey = p.ProductKey
+group by month_no, month_
+order by month_no
+
+-- 2) Problem: running total sales over month
 
 with monthly_sales as
 (select month(s.`Order Date`) as month_no, monthname(s.`Order Date`) as Month_, sum(p.`Unit Price USD`*s.Quantity) as sales
@@ -9,7 +20,7 @@ select month_no, Month_, sum(sales) over (order by month_no) as running_total_sa
 from monthly_sales 
 order by month_no;
 
--- current year sales vs previous year sales
+-- 3) Problem: current year sales vs previous year sales
 
 with yearly_sales as 
 (select year(s.`Order Date`) as year_,
@@ -19,3 +30,5 @@ on s.ProductKey = p.ProductKey
 group by year_)
 select year_, currentyear_sales, lag(currentyear_sales) over (order by year_) as prevyear_sales
 from yearly_sales;
+
+-- 
